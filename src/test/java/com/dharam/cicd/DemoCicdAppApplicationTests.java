0abb77cc0ea0ba2a2dbc6ef3cfb3249584afa72e
Dharmendra Mishra <mishra.dharmendra1987@gmail.com>
2019-12-30 13:49:34 +0100

    Create RESEARCHES.md
    Signed-off-by: Dharmendra `mishra <dharmendra.mishra@omio.com>

---
+++ src/test/java/com/dharam/cicd/DemoCicdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharam.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
